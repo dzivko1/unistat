7c3b5d7ed2ca5a564c9843e0e8c9f5f6aaa6f4fd
Dominik Živko <domy.zivko@gmail.com>
2021-08-02 12:35:50 +0200

    Add launcher class so the exported app can start properly

---
+++ src/main/kotlin/hr/ferit/dominikzivko/unistat/Launcher.kt
@@ -0,0 +1,10 @@
+package hr.ferit.dominikzivko.unistat
+
+class Launcher {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            App.main(args)
+        }
+    }
+}
